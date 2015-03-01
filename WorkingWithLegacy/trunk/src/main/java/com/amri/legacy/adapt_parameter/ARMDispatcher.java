a257df2a7abd276d915c21d46891b3224aa148a1
amri <amri@3e0ac61a-16fc-48a2-a2e6-f4da17fddde0>
2015-03-01 08:47:09 +0000

    BEFORE - Adapt Parameter

---
+++ WorkingWithLegacy/trunk/src/main/java/com/amri/legacy/adapt_parameter/ARMDispatcher.java
@@ -0,0 +1,25 @@
+package com.amri.legacy.adapt_parameter;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ARMDispatcher {
+
+	public void populate(HttpServletRequest request)
+	{
+		String[] values = request.getParameterValues(pageStateName);
+		if(values != null && values.length > 0)
+		{
+			marketBindings.put(pageStateName + getDateStamp(), values[0]);
+		}
+	}
+
+	private String pageStateName;
+	private MarketBinding marketBindings;
+	public class MarketBinding {
+		public void put(String string, String string2) {
+		}
+	}
+	private String getDateStamp() {
+		return null;
+	}
+}
